afc50b3270661fb5f05f4b65d3002932b75b4ae8
Felipe Bona <felipechiodinibona@hotmail.com>
2024-09-01 19:00:45 -0300

    instaling sanctum

--- application/Customer/Login/Controller.php
+++ application/Customer/Login/Controller.php
@@ -2,12 +2,33 @@
 
 namespace Application\Customer\Login;
 
+use App\Models\StoreCustomer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class Controller
 {
     public function __invoke(Request $request)
     {
-        // TODO: Implement __invoke() method.
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = StoreCustomer::query()
+            ->where('email', $credentials['email'])
+            ->first();
+
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => 'Usuário ou senha inválidos.',
+            ]);
+        }
+
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+
+        return response()
+            ->json(compact('user', 'token'));
     }
 }
--- application/routes.php
+++ application/routes.php
@@ -2,24 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('app')
+Route::post('login', Application\Customer\Login\Controller::class);
+
+Route::middleware('auth:sanctum')
+    ->prefix('app')
     ->group(function () {
         Route::get('home', Application\Home\Controller::class);
         Route::get('product', Application\Product\Show\Controller::class);
-
-        Route::prefix('customer')
-            ->group(function () {
-                Route::post('login', Application\Customer\Login\Controller::class);
-                Route::post('create-account', Application\Customer\CreateAccount\Controller::class);
-                Route::post('address', Application\Address\Store\Controller::class);
-            });
-
-        Route::prefix('cart')
-            ->group(function () {
-                Route::get('show', Application\Cart\Show\Controller::class);
-                Route::post('add', Application\Cart\Add\Controller::class);
-                Route::post('increase-item', Application\Cart\Increase\Controller::class);
-                Route::post('decrease-item', Application\Cart\Decrease\Controller::class);
-                Route::post('finish', Application\Cart\Finish\Controller::class);
-            });
+        Route::post('customer/create-account', Application\Customer\CreateAccount\Controller::class);
+        Route::post('customer/address', Application\Address\Store\Controller::class);
+        Route::get('cart/show', Application\Cart\Show\Controller::class);
+        Route::post('cart/add', Application\Cart\Add\Controller::class);
+        Route::post('cart/increase-item', Application\Cart\Increase\Controller::class);
+        Route::post('cart/decrease-item', Application\Cart\Decrease\Controller::class);
+        Route::post('cart/finish', Application\Cart\Finish\Controller::class);
     });
